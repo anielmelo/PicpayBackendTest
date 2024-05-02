da0901fd93a5b2f4b9698e8f8cfcd8aab4b0965c
anielmelo <anielcontato@gmail.com>
2024-05-02 17:28:21 -0300

    feat: add controllers

---
+++ app/Http/Controllers/TransferController.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Transfer;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Validation\ValidationException;
+
+class TransferController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+            $request->validate([
+                'sender'    => ['require', 'exists:users,id'],
+                'recipient' => ['require', 'exists:users,id'],
+                'amount'    => ['require', 'numeric', 'min:1'],
+            ], [
+                'sender.require'    => 'O remetente é obrigatório.',
+                'recipient.require' => 'O destinatário é obrigatório.',
+                'amount.require'    => 'O valor é obrigatório.',
+    
+                'amount.numeric'    => 'O valor deve ser um número.',
+                'amount.min'        => 'O valor deve ser no mínimo R$ :min.',
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json(['erro' => $e->validator->errors()->first()], 422);
+        }
+
+        $sender = User::findOrFail($request->sender);
+        $recipient = User::findOrFail($request->recipient);
+        $amount = $request->amount;
+
+        if ($sender->type === 'MERCHANT') {
+            return response()->json(['erro' => 'MERCHANT não pode realizar transações.'], 403);
+        }
+
+        if ($sender->balance < $amount) {
+            return response()->json(['erro' => 'Saldo insuficiente.'], 403);
+        }
+
+        DB::beginTransaction();
+        try {
+
+            $responseAuthorization = Http::get('https://run.mocky.io/v3/5794d450-d2e2-4412-8131-73d0293ac1cc');
+
+            $authorization = $responseAuthorization->json();
+
+            if ((!$responseAuthorization->successful()) || ($authorization['message'] != 'Autorizado')) {
+                return response()->json(['erro' => 'Transação não autorizada.'], 403);
+            }
+
+            $sender->balance -= $amount;
+            $sender->save();
+
+            $recipient->balance += $amount;
+            $recipient->save();
+
+            Transfer::create([
+                'sender'    => $sender->id,
+                'recipient' => $recipient->id,
+                'amount'    => $amount,
+            ]);
+
+            DB::commit();
+
+            return response()->json(['sucesso' => 'Transação realizada com sucesso.'], 201);
+        } catch (Exception $e) {
+            return response()->json(['erro' => 'Ocorreu um erro durante a transação.'], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+            $request->validate([
+                'name'     => ['required'],
+                'email'    => ['required', 'email', 'unique:users,email'],
+                'password' => ['required', 'min:8'],
+                'document' => ['required', 'unique:users,document'],
+                'balance'  => ['required', 'numeric', 'min:0'],
+                'type'     => ['required', 'in:COMMON,MERCHANT'],
+            ], [
+                'name.required'     => 'O campo nome é obrigatório.',
+                'email.required'    => 'O campo email é obrigatório.',
+                'password.required' => 'O campo senha é obrigatório.',
+                'document.required' => 'O campo documento é obrigatório.',
+                'balance.required'  => 'O campo saldo é obrigatório.',
+                'type.required'     => 'O campo tipo é obrigatório.',
+
+                'email.email'       => 'O email informado não é válido.',
+                'email.unique'      => 'Usuário já cadastrado.',
+                
+                'password.min'      => 'A senha deve ter pelo menos :min caracteres.',
+
+                'document.unique'   => 'Usuário já cadastrado.',
+                
+                'balance.numeric'   => 'O saldo deve ser um número.',
+                'balance.min'       => 'O saldo não pode ser negativo.',
+                
+                'type.in'           => 'O tipo deve ser "COMMON" ou "MERCHANT".',
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json(['erro' => $e->validator->errors()->first()], 400);
+        }
+
+        $user = new User();
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
+        $user->document = $request->document;
+        $user->balance = $request->balance;
+        $user->type = $request->type;
+
+        $user->save();
+
+        return response()->json(['sucesso' => 'Usuário criado com sucesso.'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
