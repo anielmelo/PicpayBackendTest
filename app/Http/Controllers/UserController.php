7df34c50cb8d9fcc67b703240881878e47da86b1
anielmelo <anielcontato@gmail.com>
2024-05-02 17:47:37 -0300

    fix: fix post method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,7 +14,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        // return csrf_token();
+        return 'hello world';
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\TransferController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {return 'Hello world!';});
+Route::get('/', [UserController::class, 'index']);
 
 Route::post('/users', [UserController::class, 'store']);
 Route::post('/transfers', [TransferController::class, 'store']);
