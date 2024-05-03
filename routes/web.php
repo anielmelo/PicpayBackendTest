9f72b5f5e617ba3c20110bbd93f2dca2324aedaa
anielmelo <anielcontato@gmail.com>
2024-05-03 12:50:46 -0300

    fix: fix csrf

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\TransferController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [UserController::class, 'index']);
-
+Route::get('/users', [UserController::class, 'index']);
 Route::post('/users', [UserController::class, 'store']);
 Route::post('/transfers', [TransferController::class, 'store']);
